3fbba75ac164cb9ac111e3841298b0bd1f9e10ff
Alizee Vallat <alizee.v@live.fr>
2023-02-22 18:56:36 +0100

    yelp app restaurant rails

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
 
-  before_action :set_restaurant, only: %i[show edit update destroy]
+  before_action :set_restaurant, only: %i[show]
 
   def index
     @restaurants = Restaurant.all
